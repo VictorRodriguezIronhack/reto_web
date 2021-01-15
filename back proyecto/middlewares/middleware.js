29e6bf68de06bff29bb1a0c30edf2a6878d0721c
Paricia Muñoz <patrimdm96@gmail.com>
2021-01-15 23:13:22 +0000

    naming issues

--- back proyecto/middlewares/middleware.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-
-module.exports = {
-    checkIdFormat: (req, res, next) => mongoose.Types.ObjectId.isValid(req.params.id) ? next() : res.status(404).json({ code: 404, message: 'Invalid ID' })
-}
