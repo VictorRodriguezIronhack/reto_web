3e90bc6139f4bfc83f8007662849be2a76e68a45
Sara Martínez Vega <saramartz_@hotmail.com>
2021-01-17 18:08:32 +0100

    fixing bugs

--- server/app.js
+++ server/app.js
@@ -17,12 +17,12 @@ require('./configs/middleware.config')(app)
 // Routes index
 require('./routes')(app)
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept');
+//     next();
+// });
 
 module.exports = app
--- server/configs/cors.config.js
+++ server/configs/cors.config.js
@@ -1,13 +1,24 @@
-const cors = require('cors')
+// const cors = require('cors')
 
-const whitelist = [process.env.DOMAIN_REMOTE, process.env.DOMAIN_LOCAL, 'https://phone-cave-api.herokuapp.com']
+// const whitelist = [process.env.DOMAIN_REMOTE, process.env.DOMAIN_LOCAL, '*']
 
-const corsOptions = {
-    origin: (origin, cb) => {
-        const originIsWhitelisted = whitelist.includes(origin)
-        cb(null, originIsWhitelisted)
-    }, credentials: true
+// const corsOptions = {
+//     origin: (origin, cb) => {
+//         const originIsWhitelisted = whitelist.includes(origin)
+//         cb(null, originIsWhitelisted)
+//     }, credentials: true
 
+// }
+
+// module.exports = app => app.use(cors(corsOptions))
+
+module.exports = app => {
+    app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.DOMAIN_REMOTE);
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept');
+    next();
+});
 }
 
-module.exports = app => app.use(cors(corsOptions))
