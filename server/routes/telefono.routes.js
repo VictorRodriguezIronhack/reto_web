ba0551afeaa913c3f9af797a72b7083c75948c2a
Carlos Martin-Salas Larena <carlos_2n3055@hotmail.com>
2021-01-17 11:21:32 +0100

    Create Server. Rest API

---
+++ server/routes/telefono.routes.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+
+const Phone = require('../models/phone.model')
+
+
+// ----- ENDPOINTS TELEFONOS -----
+
+
+// Muestra la lista de todos los telefonos
+router.get('/getAllPhones', (req, res) => {
+    Phone
+        .find()
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
+
+
+// Muestra los detalles de un telefono
+router.get('/getOnePhone/:phone_id', (req, res) => {
+
+    const phone_id = req.params.phone_id
+
+    Phone
+        .findById(phone_id)
+        .then(response => res.json(response))
+        .catch(err => res.status(500).json(err))
+})
+
+
+
+module.exports = router
