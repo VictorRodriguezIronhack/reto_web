5edc3e81d0c2e409bac3df525a216c85ad2fbe69
Sara Martínez Vega <saramartz_@hotmail.com>
2021-01-17 17:39:15 +0100

    changes

--- server/configs/cors.config.js
+++ server/configs/cors.config.js
@@ -1,13 +1,12 @@
 const cors = require('cors')
 
-const whitelist = [process.env.DOMAIN_REMOTE, process.env.DOMAIN_LOCAL]
+const whitelist = [process.env.DOMAIN_REMOTE, process.env.DOMAIN_LOCAL, 'https://phone-cave-api.herokuapp.com']
 
 const corsOptions = {
     origin: (origin, cb) => {
         const originIsWhitelisted = whitelist.includes(origin)
         cb(null, originIsWhitelisted)
-    },
-    credentials: true
+    }, credentials: true
 
 }
 
