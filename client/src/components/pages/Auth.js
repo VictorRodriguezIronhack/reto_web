1c411b7afbad71c05aa1314952df7b5f5a3aafb1
Jose A. Casado <jsedoviolt@gmail.com>
2021-02-16 15:57:22 +0100

    add auth form for login and signup

--- client/src/components/pages/Auth.js
+++ client/src/components/pages/Auth.js
@@ -1,11 +1,100 @@
-import React, { useState } from 'react'
+import React, { Component } from 'react'
 
-function AuthPage() {
-  // const [ count, setCount ] = useState(0)
+import './Auth.css'
 
-  return (
-    <h1>The Auth Page</h1>
-  )
+class AuthPage extends Component {
+  state = {
+    isLogin: true
+  }
+  constructor (props) {
+    super(props)
+    this.email = React.createRef()
+    this.password = React.createRef()
+  }
+
+  switchModeHandler = () => {
+    this.setState(prevState => {
+      return { isLogin: !prevState.isLogin}
+    })
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    const email = this.email.current.value
+    const password = this.password.current.value
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email }", password: "${ password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    }
+
+    if (!this.state.isLogin) {
+      requestBody = {
+        query: `
+        mutation {
+          createUser(userInput: { email: "${email }", password: "${ password}" }) {
+            _id
+            email
+          }
+        }
+        `
+      }
+    }
+
+
+    fetch('http://localhost:5000/api', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(res => {
+      if (res.status !== 200 && res.status !== 201) {
+        throw new Error('Failed!')
+      }
+      return res.json()
+    })
+      .then(resData => {
+        console.log(resData)
+      })
+      .catch(err => {
+      console.log(err)
+    })
+  }
+ 
+  render() {
+    return (
+      <form className='auth-form' onSubmit={this.submitHandler}>
+        <div className='form-control'>
+          <label htmlFor='email'>E-mail</label>
+          <input type='email' id='email' ref={ this.email }/>
+        </div>
+        
+        <div className='form-control'>
+          <label htmlFor='password'>Password</label>
+          <input type='password' id='password' ref={ this.password }/>
+        </div>
+  
+        <div className='form-actions'>
+          <button type='submit'>Submit</button>
+          <button type='button' onClick={this.switchModeHandler}>
+            Switch to {this.state.isLogin ? 'Signup' : 'Login'}
+          </button>
+        </div>
+      </form>
+    )
+  }
 }
 
 export default AuthPage
--- configs/graphql.config.js
+++ configs/graphql.config.js
@@ -5,6 +5,17 @@ const isAuth = require('../middleware/is-auth')
 
 
 module.exports = app => {
+    /* Cors config */
+    app.use((req, res, next) => {
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+        if (req.method === 'OPTIONS') {
+            return res.sendStatus(200)
+        }
+        next()
+    })
+    
     app.use(isAuth)
     
     app.use('/api', graphqlHTTP({
