2ba11b9a1b762fcb843e5866e8a6e713266d20e6
Salvador Gallego Girona <sgallegogirona@gmail.com>
2021-10-14 14:34:52 +0200

    done

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 import { useState, useEffect } from 'react'
 import { PhoneService } from './services/phone.service'
 import { PhoneCard } from './components/PhoneCard';
-import { Row, Container } from 'react-bootstrap'
+import { Row, Container, Spinner } from 'react-bootstrap'
 
 function App() {
   const ApiHandler = new PhoneService()
 
   const [phones, setPhones] = useState('')
   const [view, setView] = useState('phones')
-  const [phoneId, setPhoneId] = useState()
+  const [selectedPhone, setSelectedPhone] = useState()
+  const [loading, setLoading] = useState(true)
 
 
   const loadPhones = () => {
 
     ApiHandler.getPhones()
-      .then(response => setPhones(response.data.phones))
+      .then(response => {
+        setPhones(response.data.phones)
+
+        setTimeout(() => {
+
+          setLoading(false)
+        }, 2000);
+      })
       .catch(err => console.log(err))
 
+  }
+
+  const setId = (id) => {
+    const phone = phones.filter(elm => elm.id === id)
+    setSelectedPhone(...phone)
   }
 
 
@@ -32,24 +45,34 @@ function App() {
   return (
     <Container>
 
-      <h1 className={'text-center'}>Phones</h1>
-      <Row>
-        {
-          view === 'phones' ?
+      {loading ?
 
-            phones ?
-              phones.map(elm => {
-                return (
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+        :
+        <>
+          <h1 className={'text-center mb-5'}>{view === 'phones' ? ' Phones' : 'Phone'}</h1>
+          <Row>
+            {
+              view === 'phones' ?
 
-                  <PhoneCard {...elm} setView={setView} setPhoneId={setPhoneId} />
+                phones ?
 
-                )
-              })
-              : null
-            : null
-        }
-      </Row>
+                  phones.map(elm => {
+                    return (
 
+                      <PhoneCard key={elm.id} {...elm} setView={setView} setId={setId} view={view} />
+
+                    )
+                  })
+                  : null
+
+                : <PhoneCard  {...selectedPhone} view={view} setView={setView} />
+            }
+          </Row>
+        </>
+      }
     </Container>
   );
 }
