760da384d9e49372f567327376e3c4789c64a2da
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-10-18 17:48:14 +0200

    fixed issues with cors and added cloudinary

--- server/app.js
+++ server/app.js
@@ -1,11 +1,24 @@
 require('dotenv/config');
 require('./db');
 const express = require('express');
-
-const { isAuthenticated } = require('./middleware/jwt.middleware'); // <== IMPORT
+var cors = require('cors');
+// const { isAuthenticated } = require('./middleware/jwt.middleware'); // <== IMPORT
 
 const app = express();
 require('./config')(app);
+
+// CORS allows
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept Authorization');
+	if (req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE');
+		return res.status(200).json({});
+	}
+	next();
+});
+
+app.use(cors());
 
 // 👇 MIDDLEWARE MISSING
 const allRoutes = require('./routes');
