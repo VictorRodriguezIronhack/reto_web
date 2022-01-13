8f0a7aeaa7850753a78be9ee421d042040011e76
RaselJam <Rasel.jam.h@gmail.com>
2022-01-13 17:49:53 +0100

    frist commit, started very late, my bad, i was expecting to get news from slack, and i dindt check the email. :( going to try , only has less than 30 min left

---
+++ index.js
@@ -0,0 +1,22 @@
+const  express  = require("express");
+const  bodyParser = require( "body-parser");
+const  cors  = require("cors");
+const  dotenv  = require('dotenv');
+
+
+const app = express();
+dotenv.config();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use(function (err, req, res, next) {
+  console.log("Global Error handler,some thing didnt work:", err.message)
+  res.status(500).json({ devMesg: "global Error Handler", ErrorMessage: err.message })
+})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port': ${PORT}`))
+
+
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const router = express.Router();
+const  phonsesJson = 
+ router.get('/', (req, res)=>{
+
+ })
+
+
+export default router;
