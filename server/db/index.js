4a8dfec86b464587c31c6156c0fab3a45b89d782
Danivilla <dasvilla95@gmail.com>
2022-01-13 18:15:36 +0100

    No time

---
+++ server/db/index.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/WebChallenge";
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
