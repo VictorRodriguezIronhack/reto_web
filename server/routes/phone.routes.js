15bdafce69b5079a8c54906aecd05401b6d02a6c
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-01-17 17:58:28 +0100

    (Server): Review routes (TODO)

--- server/routes/phone.routes.js
+++ server/routes/phone.routes.js
@@ -5,6 +5,8 @@ router.get('/', (req, res) => {
   res.status(200).json(data)
 })
 
+/* TODO: Chequear si el problema de la ruta de details viene de aqui.
+Igual hay q ponerle /details/:id para no machacar el root */
 router.get('/:id', (req, res) => {
   const { id } = req.params
   const [phone] = data.filter((elm) => elm.id == id)
