11d191db15fada83fb2e0613c2c5c64ab2b1ab5e
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-01-19 22:27:24 +0100

    Deploy try number Infinite+2

--- server/app.js
+++ server/app.js
@@ -3,9 +3,12 @@ require('./db')
 
 const express = require('express')
 const app = express()
+const path = require('path')
 
 require('./config')(app)
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use('/api', require('./routes'))
-require('./error-handling')(app)
+app.use((req, res) => res.sendFile(__dirname + '/public/index.html'))
 
 module.exports = app
