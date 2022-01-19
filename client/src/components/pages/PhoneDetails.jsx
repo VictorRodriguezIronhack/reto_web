0ea8c3b007e5605d6561d1d19c65b10c45f0a0e7
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-01-19 02:01:55 +0100

    (Client): Implement Spinner and add delay to load

--- client/src/components/pages/PhoneDetails.jsx
+++ client/src/components/pages/PhoneDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import PhoneService from '../../services/phones.service'
+import Spinner from '../shared/Spinner'
 
 const phonesService = new PhoneService()
 
@@ -19,8 +20,13 @@ const PhoneDetails = () => {
     ram: 0,
   })
 
+  const [loading, setLoading] = useState(false)
+
   useEffect(() => {
-    getPhoneInfo(id)
+    setLoading(true)
+    setTimeout(() => {
+      getPhoneInfo(id)
+    }, 1500)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -28,136 +34,142 @@ const PhoneDetails = () => {
     phonesService
       .getOnePhone(id)
       .then((res) => {
-        setPhoneInfo(res.data)
+        setPhoneInfo(res.data[0])
       })
       .catch((err) => console.error(err))
+    setLoading(false)
   }
 
-  return phoneInfo[0] ? (
-    <section className="text-gray-700 body-font overflow-hidden">
-      <div className="container p-3 mx-auto rounded">
-        <div className="lg:w-4/5 mx-auto flex flex-wrap bg-white">
-          <img
-            alt="ecommerce"
-            className="lg:w-1/2 w-full px-4 object-cover object-center rounded border border-gray-200"
-            src={`/assets/images/${phoneInfo[0].imageFileName}`}
-          />
-          <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-            <h2 className="text-sm title-font text-gray-500 tracking-widest">
-              {phoneInfo[0].manufacturer}
-            </h2>
-            <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
-              {phoneInfo[0].name}
-            </h1>
-            <div className="flex mb-4">
-              <span className="flex items-center">
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 text-red-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 text-red-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 text-red-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="currentColor"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 text-red-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-4 h-4 text-red-500"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                </svg>
-                <span className="text-gray-600 ml-3">4 Reviews</span>
-              </span>
-            </div>
-            <p className="leading-relaxed">{phoneInfo[0].description}</p>
-            <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
-              <div className="flex">
-                <span className="mr-3">Color: </span>
-                <button
-                  className={`border-2 border-gray-300 ml-1 bg-${phoneInfo[0].color} rounded-full w-6 h-6 focus:outline-none`}
-                ></button>
+  return (
+    <div>
+      {loading && <Spinner />}
+      {!loading && (
+        <section className="text-gray-700 body-font overflow-hidden">
+          <div className="container p-3 mx-auto rounded">
+            <div className="lg:w-4/5 mx-auto flex flex-wrap bg-white">
+              <img
+                alt="ecommerce"
+                className="lg:w-1/2 w-full px-4 object-cover object-center rounded border border-gray-200"
+                src={`/assets/images/${phoneInfo.imageFileName}`}
+              />
+              <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                  {phoneInfo.manufacturer}
+                </h2>
+                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                  {phoneInfo.name}
+                </h1>
+                <div className="flex mb-4">
+                  <span className="flex items-center">
+                    <svg
+                      fill="currentColor"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-4 h-4 text-red-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg
+                      fill="currentColor"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-4 h-4 text-red-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg
+                      fill="currentColor"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-4 h-4 text-red-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg
+                      fill="currentColor"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-4 h-4 text-red-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-4 h-4 text-red-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                    </svg>
+                    <span className="text-gray-600 ml-3">4 Reviews</span>
+                  </span>
+                </div>
+                <p className="leading-relaxed">{phoneInfo.description}</p>
+                <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
+                  <div className="flex">
+                    <span className="mr-3">Color: </span>
+                    <button
+                      className={`border-2 border-gray-300 ml-1 bg-${phoneInfo.color} rounded-full w-6 h-6 focus:outline-none`}
+                    ></button>
+                  </div>
+                  <div className="flex ml-6 items-center">
+                    <span className="mr-3 text-sm text-gray-400 font-semibold">
+                      {phoneInfo.screen}
+                    </span>
+                  </div>
+                  <div className="flex ml-6 items-center">
+                    <span className="mr-3 text-sm text-gray-400 font-semibold">
+                      {phoneInfo.processor}
+                    </span>
+                  </div>
+                  <div className="flex ml-6 items-center">
+                    <span className="mr-3 text-sm text-gray-400 font-semibold">
+                      {phoneInfo.ram} RAM
+                    </span>
+                  </div>
+                </div>
+                <div className="flex">
+                  <span className="title-font font-medium text-4xl text-indigo-600">
+                    {phoneInfo.price} €
+                  </span>
+                  <button className="flex ml-40 bg-transparent hover:bg-indigo-200 text-indigo-600 font-semibold hover:text-white py-2 px-4 border border-indigo-500 hover:border-transparent rounded-full">
+                    Button
+                  </button>
+                  <button className="rounded-full w-10 h-10 bg-gray-200 hover:bg-red-500 p-0 border-0 inline-flex items-center justify-center text-red-500 hover:text-white ml-4">
+                    <svg
+                      fill="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      className="w-5 h-5"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                    </svg>
+                  </button>
+                </div>
               </div>
-              <div className="flex ml-6 items-center">
-                <span className="mr-3 text-sm text-gray-400 font-semibold">
-                  {phoneInfo[0].screen}
-                </span>
-              </div>
-              <div className="flex ml-6 items-center">
-                <span className="mr-3 text-sm text-gray-400 font-semibold">
-                  {phoneInfo[0].processor}
-                </span>
-              </div>
-              <div className="flex ml-6 items-center">
-                <span className="mr-3 text-sm text-gray-400 font-semibold">
-                  {phoneInfo[0].ram} RAM
-                </span>
-              </div>
-            </div>
-            <div className="flex">
-              <span className="title-font font-medium text-4xl text-indigo-600">
-                {phoneInfo[0].price} €
-              </span>
-              <button className="flex ml-40 bg-transparent hover:bg-indigo-200 text-indigo-600 font-semibold hover:text-white py-2 px-4 border border-indigo-500 hover:border-transparent rounded-full">
-                Button
-              </button>
-              <button className="rounded-full w-10 h-10 bg-gray-200 hover:bg-red-500 p-0 border-0 inline-flex items-center justify-center text-red-500 hover:text-white ml-4">
-                <svg
-                  fill="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                </svg>
-              </button>
             </div>
           </div>
-        </div>
-      </div>
-    </section>
-  ) : null
+        </section>
+      )}
+    </div>
+  )
 }
 
 export default PhoneDetails
--- client/src/components/pages/PhonePage.jsx
+++ client/src/components/pages/PhonePage.jsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import PhoneService from '../../services/phones.service'
 import PhoneList from './PhoneList'
+import Spinner from '../shared/Spinner'
 
 const phonesService = new PhoneService()
 
 const PhonePage = () => {
   const [phones, setPhones] = useState(null)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    getPhonesList()
+    setLoading(true)
+    setTimeout(() => {
+      getPhonesList()
+    }, 3000)
   }, [])
 
   const getPhonesList = () => {
@@ -16,12 +21,15 @@ const PhonePage = () => {
       .getAllPhones()
       .then((res) => setPhones(res.data))
       .catch((err) => console.error(err))
+    setLoading(false)
   }
+
   return (
     <div className="pt-2">
       <h2 className="font-bold text-center text-gray-100 text-2xl py-6">
         A list of all our phones
       </h2>
+      <div>{loading && <Spinner />}</div>
       <div className="grid lg:grid-cols-3 px-10">
         {phones?.map((elm) => (
           <PhoneList elm={elm} key={elm.id} />
