c3cb02300301760760212e693578e210a26b2c16
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-01-20 19:17:29 +0100

    (Fix): Change search button for a div

--- client/src/components/pages/SearchBar.jsx
+++ client/src/components/pages/SearchBar.jsx
@@ -21,7 +21,7 @@ const SearchBar = (props) => {
         placeholder="Search a phone..."
         onChange={handleSearch}
       />
-      <button className="sticky top-0 ml-1" onClick={handleSearch}>
+      <div className="sticky top-0 ml-1">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -36,7 +36,7 @@ const SearchBar = (props) => {
           <circle cx="11" cy="11" r="8"></circle>
           <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
         </svg>
-      </button>
+      </div>
     </div>
   )
 }
